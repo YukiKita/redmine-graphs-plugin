a0f651b6ca68902bf86828dd4473f966a91b8321
Yuki Kita <kita.yuki@gmail.com>
2010-03-06 20:07:20 +0900

    Tracker selectable at issue growth graph

--- init.rb
+++ init.rb
@@ -6,8 +6,8 @@ require_dependency 'redmine_graphs_listener'
 
 Redmine::Plugin.register :redmine_graphs do
   name 'Redmine Graphs plugin'
-  author 'Brad Beattie'
+  author 'Yuki Kita'
   description 'This plugin provides instances of Redmine with additional graphs.'
-  version '0.1.0'
+  version '0.2.0'
   settings :default=>{"tracker_ids"=>["1"]}, :partial => 'settings/graphs_settings'
 end
